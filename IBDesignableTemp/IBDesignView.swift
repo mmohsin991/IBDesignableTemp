45c01ca7f09809f319e57a200a63e0f62262b2dc
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-04-04 20:44:43 +0500

    update

--- IBDesignableTemp/IBDesignView.swift
+++ IBDesignableTemp/IBDesignView.swift
@@ -9,19 +9,70 @@
 import UIKit
 
 @IBDesignable class IBDesignView: UIView {
-
     
+    // MARK: Inspectable properties ******************************
     
-    // Only override drawRect: if you perform custom drawing.
-   //  An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-
-        var circle = UIBezierPath(arcCenter: self.center, radius: 50.0, startAngle: 0, endAngle: 360, clockwise: true)
-        UIColor.grayColor().setStroke()
-        circle.lineWidth=10
-        circle.stroke()
-        
+    @IBInspectable var startColor: UIColor = UIColor.whiteColor() {
+        didSet{
+            setupView()
+        }
     }
     
+    @IBInspectable var endColor: UIColor = UIColor.blackColor() {
+        didSet{
+            setupView()
+        }
+    }
+    
+    @IBInspectable var isHorizontal: Bool = false {
+        didSet{
+            setupView()
+        }
+    }
+    
+    @IBInspectable var roundness: CGFloat = 0.0 {
+        didSet{
+            setupView()
+        }
+    }
+    
+    
+    // Helper to return the main layer as CAGradientLayer
+    var gradientLayer: CAGradientLayer {
+        return layer as CAGradientLayer
+    }
+    
+    
+    override class func layerClass()->AnyClass{
+        return CAGradientLayer.self
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+
+
+    // Setup the view appearance
+    private func setupView(){
+        
+        let colors:Array = [startColor.CGColor, endColor.CGColor]
+        gradientLayer.colors = colors
+        gradientLayer.cornerRadius = roundness
+        
+        if (isHorizontal){
+            gradientLayer.endPoint = CGPoint(x: 1, y: 0)
+        }else{
+            gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        }
+        
+        self.setNeedsDisplay()
+        
+    }
 
 }
